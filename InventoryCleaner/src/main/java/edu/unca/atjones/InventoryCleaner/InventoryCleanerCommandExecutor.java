59c7ab62e9dca49b48b39ff3d514a52b2f554768
Asa Jones <atjones@unca.edu>
2012-10-26 11:44:43 -0400

    added command /inv clear

--- InventoryCleaner/src/main/java/edu/unca/atjones/InventoryCleaner/InventoryCleanerCommandExecutor.java
+++ InventoryCleaner/src/main/java/edu/unca/atjones/InventoryCleaner/InventoryCleanerCommandExecutor.java
@@ -3,6 +3,8 @@ package edu.unca.atjones.InventoryCleaner;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 import com.google.common.base.Joiner;
 
@@ -23,7 +25,15 @@ public class InventoryCleanerCommandExecutor implements CommandExecutor {
      * On command set the sample message
      */
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	return true;
+    	Player p = (Player)sender;
+    	if(args.length > 0) {
+	    	if(p.hasPermission("inv.clear") && args[0].equalsIgnoreCase("clear")) {
+	    		PlayerInventory i = p.getInventory();
+	    		i.clear();
+	    		return true;
+	    	}
+    	}
+    	return false;
     }
 
 }
